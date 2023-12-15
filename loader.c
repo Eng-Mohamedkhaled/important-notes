ebc7144e47fdfe151c853b92e9c0d99772ee6184
mohamedkhaled <random@gamil.com>
2023-12-15 23:04:31 +0200

    new file:   include/mylib.h 	renamed:    file.c -> lib/file.c 	new file:   lib/file.o 	renamed:    file2.c -> lib/file2.c 	new file:   lib/file2.o 	new file:   lib/libmylib.a 	new file:   loader.c 	deleted:    main.c

---
+++ loader.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+
+
+#include "mylib.h"
+
+int main () {
+	printf("%d\n", add(5,6));
+	return 0;
+
+}
--- main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include "file.c"
-#include "file2.c"
-int main () {
-	printf("allah akbar");
-	printf("\n%d\n", add(4,4));
-	printf("%d\n", sub(4,4));
-	return 0;
-}
